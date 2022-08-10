7654ff5ddec669a608403c3e9396a0b9b173853a
dadolyner <skulj.david@gmail.com>
2022-08-10 15:43:34 +0200

    auth -> switched to custom exceptions, add placeId

--- src/modules/auth/dto/auth-credentials-signup.dto.ts
+++ src/modules/auth/dto/auth-credentials-signup.dto.ts
@@ -1,23 +1,27 @@
-// Data Transfer Object for signing up
-import { IsEmail, IsString, IsUrl, MaxLength, MinLength } from 'class-validator';
+// User DTO
+import { IsEmail, IsString, IsUUID, MaxLength, MinLength } from 'class-validator'
 
 export class AuthSignUpCredentialsDto {
     @IsString()
-    first_name: string;
+    first_name: string
 
     @IsString()
-    last_name: string;
+    last_name: string
 
-    @IsEmail({ message: 'This is not an email!' })
-    email: string;
-
-    @IsString()
-    @MinLength(4, { message: 'Username must be at least 4 characters long!' })
-    @MaxLength(100, { message: 'Username is too long!' })
-    username: string;
+    @IsEmail({ message: 'This is not an email' })
+    email: string
 
     @IsString()
-    @MinLength(8, { message: 'Password is too short!' })
-    @MaxLength(100, { message: 'Password is too long!' })
-    password: string;
+    @MinLength(4, { message: 'Username must be at least 4 characters long' })
+    @MaxLength(100, { message: 'Username is too long' })
+    username: string
+
+    @IsString()
+    @MinLength(8, { message: 'Password is too short' })
+    @MaxLength(100, { message: 'Password is too long' })
+    password: string
+
+    @IsString({ message: 'placeId must be a valid uuid string' })
+    @IsUUID()
+    placeId: string
 }
