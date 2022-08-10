2ab35dae2bd58958efc2cc9a7e4cc727275e25d7
dadolyner <skulj.david@gmail.com>
2022-08-10 09:31:21 +0200

    added a exception helper function

---
+++ src/HttpException/custom.exception.ts
@@ -0,0 +1,67 @@
+import { HttpException, Logger } from "@nestjs/common";
+
+export class CustomException extends HttpException {
+    constructor(message: string, status: number) {
+        super(message, status);
+    }
+
+    static ok(location: string, message: string) {
+        const logger = new Logger(location);
+        logger.verbose(message);
+        return new CustomException(message, 200);
+    }
+
+    static created(location: string, message: string) {
+        const logger = new Logger(location);
+        logger.verbose(message);
+        return new CustomException(message, 201);
+    }
+
+    static noContent(location: string, message: string) {
+        const logger = new Logger(location);
+        logger.verbose(message);
+        return new CustomException(message, 204);
+    }
+
+    static notModified(location: string, message: string) {
+        const logger = new Logger(location);
+        logger.verbose(message);
+        return new CustomException(message, 304);
+    }
+
+    static badRequest(location: string, message: string) {
+        const logger = new Logger(location);
+        logger.error(message);
+        return new CustomException(message, 400);
+    }
+
+    static unauthorized(location: string, message: string) {
+        const logger = new Logger(location);
+        logger.error(message);
+        return new CustomException(message, 401);
+    }
+
+    static forbiden(location: string, message: string) {
+        const logger = new Logger(location);
+        logger.error(message);
+        return new CustomException(message, 403);
+    }
+
+    static notFound(location: string, message: string) {
+        const logger = new Logger(location);
+        logger.error(message);
+        return new CustomException(message, 404);
+    }
+
+    static conflict(location: string, message: string) {
+        const logger = new Logger(location);
+        logger.error(message);
+        return new CustomException(message, 409);
+    }
+
+    static internalServerError(location: string, message: string) {
+        const logger = new Logger(location);
+        logger.error(message);
+        return new CustomException(message, 505);
+    }
+}
