2aa0d2ad48ca2957e7b710ff2fb141047f2a8508
dadolyner <skulj.david@gmail.com>
2022-08-10 10:58:03 +0200

    formationg exceptions helper

--- src/HttpException/custom.exception.ts
+++ src/HttpException/custom.exception.ts
@@ -1,67 +1,67 @@
-import { HttpException, Logger } from "@nestjs/common";
+import { HttpException, Logger } from "@nestjs/common"
 
 export class CustomException extends HttpException {
     constructor(message: string, status: number) {
-        super(message, status);
+        super(message, status)
     }
 
     static ok(location: string, message: string) {
-        const logger = new Logger(location);
-        logger.verbose(message);
-        return new CustomException(message, 200);
+        const logger = new Logger(location)
+        logger.verbose(message)
+        return new CustomException(message, 200)
     }
 
     static created(location: string, message: string) {
-        const logger = new Logger(location);
-        logger.verbose(message);
-        return new CustomException(message, 201);
+        const logger = new Logger(location)
+        logger.verbose(message)
+        return new CustomException(message, 201)
     }
 
     static noContent(location: string, message: string) {
-        const logger = new Logger(location);
-        logger.verbose(message);
-        return new CustomException(message, 204);
+        const logger = new Logger(location)
+        logger.verbose(message)
+        return new CustomException(message, 204)
     }
 
     static notModified(location: string, message: string) {
-        const logger = new Logger(location);
-        logger.verbose(message);
-        return new CustomException(message, 304);
+        const logger = new Logger(location)
+        logger.verbose(message)
+        return new CustomException(message, 304)
     }
 
     static badRequest(location: string, message: string) {
-        const logger = new Logger(location);
-        logger.error(message);
-        return new CustomException(message, 400);
+        const logger = new Logger(location)
+        logger.error(message)
+        return new CustomException(message, 400)
     }
 
     static unauthorized(location: string, message: string) {
-        const logger = new Logger(location);
-        logger.error(message);
-        return new CustomException(message, 401);
+        const logger = new Logger(location)
+        logger.error(message)
+        return new CustomException(message, 401)
     }
 
     static forbiden(location: string, message: string) {
-        const logger = new Logger(location);
-        logger.error(message);
-        return new CustomException(message, 403);
+        const logger = new Logger(location)
+        logger.error(message)
+        return new CustomException(message, 403)
     }
 
     static notFound(location: string, message: string) {
-        const logger = new Logger(location);
-        logger.error(message);
-        return new CustomException(message, 404);
+        const logger = new Logger(location)
+        logger.error(message)
+        return new CustomException(message, 404)
     }
 
     static conflict(location: string, message: string) {
-        const logger = new Logger(location);
-        logger.error(message);
-        return new CustomException(message, 409);
+        const logger = new Logger(location)
+        logger.error(message)
+        return new CustomException(message, 409)
     }
 
     static internalServerError(location: string, message: string) {
-        const logger = new Logger(location);
-        logger.error(message);
-        return new CustomException(message, 505);
+        const logger = new Logger(location)
+        logger.error(message)
+        return new CustomException(message, 505)
     }
 }
