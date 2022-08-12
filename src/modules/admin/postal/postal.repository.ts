c89ac1ea5b750a3c3bddfb4497a8aeb7e009ba91
dadolyner <skulj.david@gmail.com>
2022-08-12 10:04:11 +0200

    postal - error handling, pretty data output

--- src/modules/admin/postal/postal.repository.ts
+++ src/modules/admin/postal/postal.repository.ts
@@ -11,49 +11,49 @@ export class PostalRepository extends Repository<Postals> {
     async createPostal(postalDto: CreatePostalDto): Promise<void> {
         const { post_office, post_number } = postalDto
 
+        const postalExists = await this.findOne({ where: [{ post_office }, { post_number }] })
+        if (postalExists) throw CustomException.badRequest(PostalRepository.name, `Postal ${post_office} (${post_number}) already exists.`)
+
         const newPostal = new Postals()
         newPostal.post_office = post_office
         newPostal.post_number = post_number
         newPostal.created_at = new Date()
         newPostal.updated_at = new Date()
 
-        const postalExists = await this.findOne({ where: { post_office, post_number } })
-        if (postalExists) throw CustomException.badRequest(PostalRepository.name, `Postal: ${post_office} (${post_number}) already exists`)
-
         try { await newPostal.save() }
-        catch (error) { throw CustomException.internalServerError(PostalRepository.name, `Adding a postal failed!. Reason: ${error.message}`) }
+        catch (error) { throw CustomException.internalServerError(PostalRepository.name, `Adding a postal failed. Reason: ${error.message}.`) }
 
-        throw CustomException.created(PostalRepository.name, `Postal: ${post_office} (${post_number}) successfully created!`)
+        throw CustomException.created(PostalRepository.name, `Postal ${post_office} (${post_number}) successfully created.`)
     }
 
     // Edit Postal
     async editPostal(postalId: string, postalDto: CreatePostalDto): Promise<void> {
+        const { post_office, post_number } = postalDto
+        
         const existingPostal = await this.findOne({ where: { id: postalId } })
-        if (!existingPostal) throw CustomException.conflict(PostalRepository.name, `Postal with id: ${postalId} does not exist!`)
+        if (!existingPostal) throw CustomException.conflict(PostalRepository.name, `Provided postal does not exist.`)
+        const postalExists = await this.findOne({ where: [{ post_office }, { post_number }] })
+        if (postalExists) throw CustomException.badRequest(PostalRepository.name, `Postal ${post_office} (${post_number}) already exists.`)
 
         const oldPostal = { ...existingPostal }
-        const { post_office, post_number } = postalDto
         existingPostal.post_office = post_office
         existingPostal.post_number = post_number
         existingPostal.updated_at = new Date()
 
-        const postalExists = await this.findOne({ where: { post_office, post_number } })
-        if (postalExists) throw CustomException.badRequest(PostalRepository.name, `Postal: ${post_office} (${post_number}) already exists`)
-
         try { await existingPostal.save() }
-        catch (error) { throw CustomException.internalServerError(PostalRepository.name, `Editing a postal failed!. Reason: ${error.message}`) }
+        catch (error) { throw CustomException.internalServerError(PostalRepository.name, `Editing a postal failed. Reason: ${error.message}.`) }
 
-        throw CustomException.ok(PostalRepository.name, `Postal: ${oldPostal.post_office} (${oldPostal.post_number}) successfully changed into ${post_office} (${post_number})!`)
+        throw CustomException.ok(PostalRepository.name, `Postal ${oldPostal.post_office} (${oldPostal.post_number}) successfully edited.`)
     }
 
     // Delete Postal
     async deletePostal(postalId: string): Promise<void> {
         const existingPostal = await this.findOne({ where: { id: postalId } })
-        if (!existingPostal) throw CustomException.conflict(PostalRepository.name, `Postal with id: ${postalId} does not exist!`)
-        
-        try { await existingPostal.remove() }
-        catch (error) { throw CustomException.internalServerError(PostalRepository.name, `Deleting a postal failed!. Reason: ${error.message}`) }
+        if (!existingPostal) throw CustomException.conflict(PostalRepository.name, `Provided postal does not exist.`)
 
-        throw CustomException.ok(PostalRepository.name, `Postal: ${existingPostal.post_office} (${existingPostal.post_number}) successfully deleted!`)
+        try { await existingPostal.remove() }
+        catch (error) { throw CustomException.internalServerError(PostalRepository.name, `Deleting a postal failed. Reason: ${error.message}.`) }
+
+        throw CustomException.ok(PostalRepository.name, `Postal ${existingPostal.post_office} (${existingPostal.post_number}) successfully deleted.`)
     }
 }
--- src/modules/admin/postal/postal.service.ts
+++ src/modules/admin/postal/postal.service.ts
@@ -2,29 +2,32 @@
 import { Injectable, Logger } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Postals } from 'src/entities/postals.entity'
+import { CustomException } from 'src/helpers/custom.exception'
 import { CreatePostalDto } from './dto/create-postal.dto'
 import { PostalRepository } from './postal.repository'
 
 @Injectable()
 export class PostalService {
     private readonly logger = new Logger(PostalService.name)
-    constructor(@InjectRepository(PostalRepository) private readonly postalRepository: PostalRepository) {}
+    constructor(@InjectRepository(PostalRepository) private readonly postalRepository: PostalRepository) { }
 
     // Get Postals
     async getPostals(postalFilters: string): Promise<Postals[]> {
-        const postalsQuery = this.postalRepository.createQueryBuilder()
-            .select([
-                'postal.id',
-                'postal.post_office',
-                'postal.post_number',
-            ])
-            .from(Postals, 'postal')
-            .where(postalFilters)
+        try {
+            const postals = await this.postalRepository.createQueryBuilder()
+                .select([
+                    'postal.id',
+                    'postal.post_office',
+                    'postal.post_number',
+                ])
+                .from(Postals, 'postal')
+                .where(postalFilters)
+                .getMany()
 
-        const postals = await postalsQuery.getMany()
-        this.logger.verbose(`Retrieving postals. Found ${postals.length} items.`)
-
-        return postals
+            this.logger.verbose(`Retrieving postals. Found ${postals.length} items.`)
+            return postals
+        }
+        catch (error) { throw CustomException.internalServerError(PostalService.name, `Retrieving postals failed. Reason: ${error.message}.`) }
     }
 
     // Create Postal
