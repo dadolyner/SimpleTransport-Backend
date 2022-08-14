59c093b31e978b3429e13260316a32741ab0b673
dadolyner <skulj.david@gmail.com>
2022-08-14 22:22:16 +0200

    edit all imports in app.module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,19 +1,21 @@
-import { UserModule } from './user/user.module';
-import { PostalModule } from './admin/postal/postal.module';
-import { PlaceModule } from './admin/place/place.module';
-import { ModelModule } from './admin/model/model.module';
-import { FuelModule } from './admin/fuel/fuel.module';
-import { CountryModule } from './admin/country/country.module';
-import { ColorModule } from './admin/color/color.module';
-import { BrandModule } from './admin/brand/brand.module';
-import { TypeOrmConfig } from 'src/config/config.typeorm';
-import { AuthModule } from './auth/auth.module';
-import { VehicleModule } from './vehicle/vehicle.module';
-import { RentalModule } from './rental/rental.module';
-import { Module } from '@nestjs/common';
+import { ImageModule } from './admin/image/image.module'
+import { UserModule } from './user/user.module'
+import { PostalModule } from './admin/postal/postal.module'
+import { PlaceModule } from './admin/place/place.module'
+import { ModelModule } from './admin/model/model.module'
+import { FuelModule } from './admin/fuel/fuel.module'
+import { CountryModule } from './admin/country/country.module'
+import { ColorModule } from './admin/color/color.module'
+import { BrandModule } from './admin/brand/brand.module'
+import { TypeOrmConfig } from 'src/config/config.typeorm'
+import { AuthModule } from './auth/auth.module'
+import { VehicleModule } from './vehicle/vehicle.module'
+import { RentalModule } from './rental/rental.module'
+import { Module } from '@nestjs/common'
 
 @Module({
     imports: [
+        ImageModule,
         TypeOrmConfig,
         AuthModule,
         UserModule,
