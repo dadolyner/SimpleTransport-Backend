a71e653b2efac395f9d96469bdaa8fa15887daef
dadolyner <skulj.david@gmail.com>
2022-08-16 14:46:14 +0200

    helper function for typeorm filters

---
+++ src/helpers/queryFilter.ts
@@ -0,0 +1,11 @@
+// Helper function that gets query as parameter and return proper TypeORM filters
+export const QueryFilters = (Query: string): [string, object] => {
+    const primaryKeys: string[] = Object.keys(Query)
+    const primaryValues: string[] = Object.values(Query)
+    const lastKeys: string[] = primaryKeys.map(key => { return key.split('.').pop() })
+
+    const filter: string = primaryKeys.map((key, index) => { return `${key} = :${lastKeys[index]}` }).join(' AND ')
+    const object: object = primaryKeys.map((key, index) => { return { [lastKeys[index]]: primaryValues[index] } }).reduce((key, value) => Object.assign(key, value), {})
+
+    return [filter, object]
+}
