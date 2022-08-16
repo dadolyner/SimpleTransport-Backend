e0c17a10b015559bc4de5d812d71aaa42a473ab5
dadolyner <skulj.david@gmail.com>
2022-08-16 15:39:55 +0200

    setup swagger docs, need to doc everything

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Logger, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app.module'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
@@ -10,6 +11,15 @@ const SimpleTransport = async () => {
         const logger = new Logger('SimpleTransport')
         const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), { cors: true })
         app.useGlobalPipes(new ValidationPipe())
+
+        const swaggerConfig = new DocumentBuilder()
+            .setTitle('Simple Transport')
+            .setDescription('API Documentation for Simple Transport project.')
+            .setVersion('1.0')
+            .build()
+        const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
+        SwaggerModule.setup('docs', app, swaggerDocument)
+
         await app.listen(3000, '0.0.0.0')
         logger.log('Application SimpleTransport is listening on port http://localhost:3000')
     } catch (error) {
