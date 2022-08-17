d8872113d7e170f9e833dc6713e45e5bface4f72
dadolyner <skulj.david@gmail.com>
2022-08-17 11:21:33 +0200

    model documentation

--- src/modules/admin/model/dto/create-model.dto.ts
+++ src/modules/admin/model/dto/create-model.dto.ts
@@ -1,11 +1,14 @@
 // Model DTO
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString, IsUUID } from "class-validator"
 
 export class CreateModelDto {
+    @ApiProperty({ type: String, description: 'Model name' })
     @IsString({ message: 'Model must be a string' })
     @IsNotEmpty({ message: 'Model is required' })
     model: string
 
+    @ApiProperty({ type: String, description: 'Brand id' })
     @IsString({ message: 'brandId must be a valid uuid string' })
     @IsUUID()
     brandId: string
--- src/modules/admin/model/model.controller.ts
+++ src/modules/admin/model/model.controller.ts
@@ -1,5 +1,6 @@
 // Model Controller
 import { Body, Query, Controller, Get, Post, Patch, Delete } from '@nestjs/common'
+import { ApiBody, ApiQuery, ApiResponse } from '@nestjs/swagger'
 import { ModelsOutput } from 'src/interfaces/model-output.interface'
 import { CreateModelDto } from './dto/create-model.dto'
 import { ModelService } from './model.service'
@@ -9,24 +10,32 @@ export class ModelController {
     constructor(private readonly modelService: ModelService) {}
 
     // Get Models
+    @ApiResponse({ status: 200, description: 'Retrieve all models' })
     @Get()
     async getModels(@Query() modelFilters: string): Promise<ModelsOutput[]> {
         return this.modelService.getModels(modelFilters)
     }
 
     // Create Model
+    @ApiResponse({ status: 201, description: 'Create a new model' })
+    @ApiBody({ type: CreateModelDto })
     @Post()
     async createModel(@Body() modelDto: CreateModelDto): Promise<void> {
         return this.modelService.createModel(modelDto)
     }
 
     // Edit Model
+    @ApiResponse({ status: 200, description: 'Edit existing model' })
+    @ApiQuery({ name: 'id', description: 'model id', required: true })
+    @ApiBody({ type: CreateModelDto })
     @Patch()
     async editModel(@Query('id') modelId: string, @Body() modelDto: CreateModelDto): Promise<void> {
         return this.modelService.editModel(modelId, modelDto)
     }
 
     // Delete Model
+    @ApiResponse({ status: 200, description: 'Delete existing model' })
+    @ApiQuery({ name: 'id', description: 'model id', required: true })
     @Delete()
     async deleteModel(@Query('id') modelId: string): Promise<void> {
         return this.modelService.deleteModel(modelId)
