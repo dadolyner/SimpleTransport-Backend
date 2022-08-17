03cdadb9b6316fd625655155f7467e709528d131
dadolyner <skulj.david@gmail.com>
2022-08-17 09:55:16 +0200

    rental documentation

--- src/modules/rental/dto/create-rental.dto.ts
+++ src/modules/rental/dto/create-rental.dto.ts
@@ -1,15 +1,19 @@
 // Rental DTO
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString, IsUUID } from "class-validator"
 
 export class CreateRentalDto {
+    @ApiProperty({ type: String, description: 'Rental start date format(YYYY-MM-DD HH:mm)' })
     @IsString({ message: "rent_start must be a string" })
     @IsNotEmpty({ message: "rent_start is required" })
     rent_start: string
 
+    @ApiProperty({ type: String, description: 'Rental end date format(YYYY-MM-DD HH:mm)' })
     @IsString({ message: "rent_end must be a string" })
     @IsNotEmpty({ message: "rent_end is required" })
     rent_end: string
 
+    @ApiProperty({ type: String, description: 'Rental vehicleId' })
     @IsString({ message: "vehicle_id must be a valid uuid string" })
     @IsUUID()
     vehicleId: string
--- src/modules/rental/rental.controller.ts
+++ src/modules/rental/rental.controller.ts
@@ -1,5 +1,6 @@
 // Rental Controller
 import { Body, Controller, Delete, Get, Patch, Post, Query } from '@nestjs/common'
+import { ApiBearerAuth, ApiBody, ApiQuery, ApiResponse } from '@nestjs/swagger'
 import { Users } from 'src/entities/users.entity'
 import { RentalsOutput } from 'src/interfaces/rental-output.interface'
 import { GetUser } from '../auth/decorator/get-user.decorator'
@@ -11,24 +12,34 @@ export class RentalController {
     constructor(private readonly rentalService: RentalService) {}
 
     // Get Rentals
+    @ApiResponse({ status: 200, description: 'Retrieve all rentals' })
     @Get()
     async getRentals(@Query() rentalFilters: string): Promise<RentalsOutput[]> {
         return this.rentalService.getRentals(rentalFilters)
     }
 
     // Create Rental
+    @ApiResponse({ status: 201, description: 'Create a new rental' })
+    @ApiBody({ type: CreateRentalDto })
+    @ApiBearerAuth()
     @Post()
     async createRental(@GetUser() user: Users, @Body() createRentalDto: CreateRentalDto): Promise<void> {
         return this.rentalService.createRental(user, createRentalDto)
     }
 
     // Edit Rental
+    @ApiResponse({ status: 200, description: 'Edit existing rental' })
+    @ApiQuery({ name: 'id', description: 'rental id', required: true })
+    @ApiBody({ type: CreateRentalDto })
+    @ApiBearerAuth()
     @Patch()
     async editRental(@GetUser() user: Users, @Query('id') rentalId: string, @Body() editRentalDto: CreateRentalDto): Promise<void> {
         return this.rentalService.editRental(user, rentalId, editRentalDto)
     }
 
     // Delete Rental
+    @ApiResponse({ status: 200, description: 'Delete existing rental' })
+    @ApiQuery({ name: 'id', description: 'rental id', required: true })
     @Delete()
     async deleteRental(@Query('id') rentalId: string): Promise<void> {
         return this.rentalService.deleteRental(rentalId)
