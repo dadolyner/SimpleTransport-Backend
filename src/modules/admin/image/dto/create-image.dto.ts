9cd1c821a7bd58cebf789b529ab8eb044cedc450
dadolyner <skulj.david@gmail.com>
2022-08-17 09:33:38 +0200

    image documentation

--- src/modules/admin/image/dto/create-image.dto.ts
+++ src/modules/admin/image/dto/create-image.dto.ts
@@ -1,7 +1,9 @@
 // Image DTO
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString } from "class-validator"
 
 export class CreateImageDto {
+    @ApiProperty({ type: String, description: 'Image url' })
     @IsString({ message: 'Image url is not a string' })
     @IsNotEmpty({ message: 'Image url is required' })
     url: string
--- src/modules/admin/image/image.controller.ts
+++ src/modules/admin/image/image.controller.ts
@@ -1,5 +1,6 @@
 // Image Controller
 import { Body, Controller, Delete, Get, Patch, Post, Query } from '@nestjs/common'
+import { ApiBody, ApiQuery, ApiResponse } from '@nestjs/swagger'
 import { Images } from 'src/entities/images.entity'
 import { CreateImageDto } from './dto/create-image.dto'
 import { ImageService } from './image.service'
@@ -9,24 +10,34 @@ export class ImageController {
     constructor(private readonly imageService: ImageService) {}
 
     // Get Images
+    @ApiResponse({ status: 200, description: 'Retrieve all or filtered images.' })
+    @ApiQuery({ name: 'image.id', description: 'image id', required: false })
+    @ApiQuery({ name: 'image.url', description: 'image url', required: false })
     @Get()
     async getImages(@Query() imageFilters: string): Promise<Images[]> {
         return await this.imageService.getImages(imageFilters)
     }
 
     // Create Image
+    @ApiResponse({ status: 201, description: 'Create new image.' })
+    @ApiBody({ type: CreateImageDto })
     @Post()
     async createImage(@Body() imageDto: CreateImageDto): Promise<void> {
         await this.imageService.createImage(imageDto)
     }
 
     // Edit Image
+    @ApiResponse({ status: 200, description: 'Edit existing image.' })
+    @ApiQuery({ name: 'id', description: 'image id', required: true })
+    @ApiBody({ type: CreateImageDto })
     @Patch()
     async editImage(@Query('id') imageId: string, @Body() imageDto: CreateImageDto): Promise<void> {
         await this.imageService.editImage(imageId, imageDto)
     }
 
     // Delete Image
+    @ApiResponse({ status: 200, description: 'Delete existing image.' })
+    @ApiQuery({ name: 'id', description: 'image id', required: true })
     @Delete()
     async deleteImage(@Query('id') imageId: string): Promise<void> {
         await this.imageService.deleteImage(imageId)
