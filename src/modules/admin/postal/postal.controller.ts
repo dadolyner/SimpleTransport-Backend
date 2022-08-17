c1d05ca2b163fb7962bcf60a90b1e63492ab32c3
dadolyner <skulj.david@gmail.com>
2022-08-17 10:37:39 +0200

    postal documentation

--- src/modules/admin/postal/postal.controller.ts
+++ src/modules/admin/postal/postal.controller.ts
@@ -1,5 +1,6 @@
 // Postal Controller
 import { Body, Controller, Delete, Get, Patch, Post, Query } from '@nestjs/common'
+import { ApiBody, ApiQuery, ApiResponse } from '@nestjs/swagger'
 import { Postals } from 'src/entities/postals.entity'
 import { CreatePostalDto } from './dto/create-postal.dto'
 import { PostalService } from './postal.service'
@@ -9,24 +10,32 @@ export class PostalController {
     constructor(private readonly postalService: PostalService) {}
 
     // Get Postals
+    @ApiResponse({ status: 200, description: 'Retrieve all postals' })
     @Get()
     async getPostals(@Query() postalFilters: string): Promise<Postals[]> {
         return this.postalService.getPostals(postalFilters)
     }
 
     // Create Postal
+    @ApiResponse({ status: 201, description: 'Create a new postal' })
+    @ApiBody({ type: CreatePostalDto })
     @Post()
     async createPostal(@Body() postalDto: CreatePostalDto): Promise<void> {
         return this.postalService.createPostal(postalDto)
     }
 
     // Edit Postal
+    @ApiResponse({ status: 200, description: 'Edit existing postal' })
+    @ApiQuery({ name: 'id', description: 'place id', required: true })
+    @ApiBody({ type: CreatePostalDto })
     @Patch()
     async editPostal(@Query('id') postalId: string, @Body() postalDto: CreatePostalDto): Promise<void> {
         return this.postalService.editPostal(postalId, postalDto)
     }
 
     // Delete Postal
+    @ApiResponse({ status: 200, description: 'Delete existing postal' })
+    @ApiQuery({ name: 'id', description: 'place id', required: true })
     @Delete()
     async deletePostal(@Query('id') postalId: string): Promise<void> {
         return this.postalService.deletePostal(postalId)
