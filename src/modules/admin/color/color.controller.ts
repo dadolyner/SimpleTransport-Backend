ba5e1f70d6974f3f9629f8ea70352622175e3e5f
dadolyner <skulj.david@gmail.com>
2022-08-17 11:55:22 +0200

    color documentation

--- src/modules/admin/color/color.controller.ts
+++ src/modules/admin/color/color.controller.ts
@@ -1,5 +1,6 @@
 // Color Controller
 import { Body, Controller, Delete, Get, Patch, Post, Query } from '@nestjs/common'
+import { ApiBody, ApiQuery, ApiResponse } from '@nestjs/swagger'
 import { Colors } from 'src/entities/colors.entity'
 import { ColorService } from './color.service'
 import { CreateColorDto } from './dto/create-color.dto'
@@ -9,24 +10,32 @@ export class ColorController {
     constructor(private readonly colorService: ColorService) { }
 
     // Get All Colors
+    @ApiResponse({ status: 200, description: 'Get all colors' })
     @Get()
     async getAllColors(@Query() colorFilters: string): Promise<Colors[]> {
         return this.colorService.getColors(colorFilters)
     }
 
     // Create Color
+    @ApiResponse({ status: 201, description: 'Create a new color' })
+    @ApiBody({ type: CreateColorDto })
     @Post()
     async createColor(@Body() colorDto: CreateColorDto): Promise<void> {
         return this.colorService.createColor(colorDto)
     }
 
     // Edit Color
+    @ApiResponse({ status: 200, description: 'Edit existing color' })
+    @ApiQuery({ name: 'id', description: 'color id', required: true })
+    @ApiBody({ type: CreateColorDto })
     @Patch()
     async editColor(@Query('id') colorId: string, @Body() colorDto: CreateColorDto): Promise<void> {
         return this.colorService.editColor(colorId, colorDto)
     }
 
     // Delete Color
+    @ApiResponse({ status: 200, description: 'Delete existing color' })
+    @ApiQuery({ name: 'id', description: 'color id', required: true })
     @Delete()
     async deleteColor(@Query('id') colorId: string): Promise<void> {
         return this.colorService.deleteColor(colorId)
--- src/modules/admin/color/dto/create-color.dto.ts
+++ src/modules/admin/color/dto/create-color.dto.ts
@@ -1,7 +1,9 @@
 // Color DTO
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString } from "class-validator"
 
 export class CreateColorDto {
+    @ApiProperty({ type: String, description: 'Color Name' })
     @IsString({ message: "Color must be a string"})
     @IsNotEmpty({ message: "Color is required" })
     color: string
