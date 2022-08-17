4d63d400b62fc4f8ea10ea63c30ce8f5e75243bf
dadolyner <skulj.david@gmail.com>
2022-08-17 09:50:46 +0200

    vehicle documentation

--- src/modules/vehicle/dto/create-vehicle.dto.ts
+++ src/modules/vehicle/dto/create-vehicle.dto.ts
@@ -1,54 +1,68 @@
 // Vehicle DTO
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsNumber, IsString, IsUUID } from "class-validator"
 
 export class CreateVehicleDto {
+    @ApiProperty({ type: Number, description: 'Vehicle number of seats' })
     @IsNumber()
     @IsNotEmpty({message: "Seats are required"})
     seats: number
 
+    @ApiProperty({ type: String, description: 'Vehicle type of shifter' })
     @IsString({ message: "Shifter must be a string" })
     @IsNotEmpty({message: "Shifter is required"})
     shifter: string
 
+    @ApiProperty({ type: Number, description: 'Vehicle horsepower' })
     @IsNumber()
     @IsNotEmpty({message: "Horsepower is required"})
     horsepower: number
 
+    @ApiProperty({ type: Number, description: 'Vehicle torque' })
     @IsNumber()
     @IsNotEmpty({message: "Torque is required"})
     torque: number
 
+    @ApiProperty({ type: Number, description: 'Vehicle acceleration' })
     @IsNumber()
     @IsNotEmpty({message: "Acceleration is required"})
     acceleration: number
 
+    @ApiProperty({ type: String, description: 'Vehicle year' })
     @IsString({ message: "Year must be a string" })
     year: string
 
+    @ApiProperty({ type: Number, description: 'Vehicle price' })
     @IsNumber()
     @IsNotEmpty({message: "Price is required"})
     price: number
 
+    @ApiProperty({ type: Number, description: 'Vehicle rent duration' })
     @IsNumber()
     @IsNotEmpty({message: "Rent duration plate is required"})
     rent_duration: number
 
+    @ApiProperty({ type: String, description: 'Vehicle licence plate' })
     @IsString({ message: "Licence Plate must be a string" })
     @IsNotEmpty({message: "Licence is required"})
     licence_plate: string
 
+    @ApiProperty({ type: String, description: 'Vehicle vin' })
     @IsString({ message: "Vin must be a string" })
     @IsNotEmpty({message: "Vin is required"})
     vin: string
 
+    @ApiProperty({ type: String, description: 'Vehicle modelId' })
     @IsString({ message: "Model must be a valid uuid string" })
     @IsUUID()
     modelId: string
 
+    @ApiProperty({ type: String, description: 'Vehicle colorId' })
     @IsString({ message: "Color must be a valid uuid string" })
     @IsUUID()
     colorId: string
 
+    @ApiProperty({ type: String, description: 'Vehicle fuelId' })
     @IsString({ message: "Fuel must be a valid uuid string" })
     @IsUUID()
     fuelId: string
--- src/modules/vehicle/vehicle.controller.ts
+++ src/modules/vehicle/vehicle.controller.ts
@@ -1,5 +1,6 @@
 // Vehicle Controller
 import { Body, Controller, Delete, Get, Patch, Post, Query } from '@nestjs/common'
+import { ApiBearerAuth, ApiBody, ApiQuery, ApiResponse } from '@nestjs/swagger'
 import { Users } from 'src/entities/users.entity'
 import { VehiclesOutput } from 'src/interfaces/vehicle-output.interface'
 import { GetUser } from '../auth/decorator/get-user.decorator'
@@ -8,29 +9,39 @@ import { VehicleService } from './vehicle.service'
 
 @Controller('vehicle')
 export class VehicleController {
-    constructor(private readonly vehicleService: VehicleService) {}
+    constructor(private readonly vehicleService: VehicleService) { }
 
     // Get Vehicles
+    @ApiResponse({ status: 200, description: 'Retrieve all vehicles.' })
     @Get()
     async getVehicles(@Query() vehicleFilters: string): Promise<VehiclesOutput[]> {
         return await this.vehicleService.getVehicles(vehicleFilters)
     }
 
     // Create Vehicle
+    @ApiResponse({ status: 201, description: 'Create new vehicle.' })
+    @ApiBody({ type: CreateVehicleDto })
+    @ApiBearerAuth()
     @Post()
     async createVehicle(@GetUser() user: Users, @Body() vehicleDto: CreateVehicleDto): Promise<void> {
         return await this.vehicleService.createVehicle(user, vehicleDto)
     }
 
     // Edit Vehicle
+    @ApiResponse({ status: 200, description: 'Edit existing vehicle.' })
+    @ApiQuery({ name: 'id', description: 'vehicle id', required: true })
+    @ApiBody({ type: CreateVehicleDto })
+    @ApiBearerAuth()
     @Patch()
-    async editVehicle(@GetUser() user: Users, @Query('id') vehicleId:string, @Body() vehicleDto: CreateVehicleDto): Promise<void> {
-        return await this.vehicleService.editVehicle(user, vehicleId,vehicleDto)
+    async editVehicle(@GetUser() user: Users, @Query('id') vehicleId: string, @Body() vehicleDto: CreateVehicleDto): Promise<void> {
+        return await this.vehicleService.editVehicle(user, vehicleId, vehicleDto)
     }
 
     // Delete Vehicle
+    @ApiResponse({ status: 200, description: 'Delete existing vehicle.' })
+    @ApiQuery({ name: 'id', description: 'vehicle id', required: true })
     @Delete()
-    async deleteVehicle(@Query('id') vehicleId:string): Promise<void> {
+    async deleteVehicle(@Query('id') vehicleId: string): Promise<void> {
         return await this.vehicleService.deleteVehicle(vehicleId)
     }
 }
