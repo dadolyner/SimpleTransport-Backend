051eb634dbad0b60f28cb8a71c0840e2e01a8271
dadolyner <skulj.david@gmail.com>
2022-08-17 11:36:16 +0200

    fuel documentation

--- src/modules/admin/fuel/dto/create-fuel.dto.ts
+++ src/modules/admin/fuel/dto/create-fuel.dto.ts
@@ -1,7 +1,9 @@
 // Fuel DTO
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString } from "class-validator"
 
 export class CreateFuelDto {
+    @ApiProperty({ type: String, description: 'Fuel name' })
     @IsString({ message: "Fuel must be a string" })
     @IsNotEmpty({ message: "Fuel is required" })
     fuel: string
--- src/modules/admin/fuel/fuel.controller.ts
+++ src/modules/admin/fuel/fuel.controller.ts
@@ -1,31 +1,40 @@
 // Fuel Controller
 import { Body, Controller, Delete, Get, Patch, Post, Query } from '@nestjs/common'
+import { ApiBody, ApiQuery, ApiResponse } from '@nestjs/swagger'
 import { Fuels } from 'src/entities/fuels.entity'
 import { CreateFuelDto } from './dto/create-fuel.dto'
 import { FuelService } from './fuel.service'
 @Controller('fuel')
 export class FuelController {
-    constructor(private readonly fuelService: FuelService) {}
+    constructor(private readonly fuelService: FuelService) { }
 
     // Get Fuels
+    @ApiResponse({ status: 200, description: 'Retrieve all fuels' })
     @Get()
     async getFuels(@Query() fuelFilters: string): Promise<Fuels[]> {
         return await this.fuelService.getFuels(fuelFilters)
     }
 
     // Create Fuel
+    @ApiResponse({ status: 201, description: 'Create a new fuel' })
+    @ApiBody({ type: CreateFuelDto })
     @Post()
     async createFuel(@Body() fuelDto: CreateFuelDto): Promise<void> {
         await this.fuelService.createFuel(fuelDto)
     }
 
     // Edit Fuel
+    @ApiResponse({ status: 200, description: 'Edit existing fuel' })
+    @ApiQuery({ name: 'id', description: 'fuel id', required: true })
+    @ApiBody({ type: CreateFuelDto })
     @Patch()
     async editFuel(@Query('id') fuelId: string, @Body() fuelDto: CreateFuelDto): Promise<void> {
         await this.fuelService.editFuel(fuelId, fuelDto)
     }
 
     // Delete Fuel
+    @ApiResponse({ status: 200, description: 'Delete existing fuel' })
+    @ApiQuery({ name: 'id', description: 'fuel id', required: true })
     @Delete()
     async deleteFuel(@Query('id') fuelId: string): Promise<void> {
         await this.fuelService.deleteFuel(fuelId)
