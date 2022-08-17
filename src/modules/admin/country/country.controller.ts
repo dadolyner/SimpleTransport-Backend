966ee60545f617261af7146c2cb87e623f6c012e
dadolyner <skulj.david@gmail.com>
2022-08-17 10:01:34 +0200

    country documentation

--- src/modules/admin/country/country.controller.ts
+++ src/modules/admin/country/country.controller.ts
@@ -1,5 +1,6 @@
 // Country Controller
 import { Body, Controller, Delete, Get, Patch, Post, Query } from '@nestjs/common'
+import { ApiBody, ApiQuery, ApiResponse } from '@nestjs/swagger'
 import { Countries } from 'src/entities/countries.entity'
 import { CountryService } from './country.service'
 import { CreateCountryDto } from './dto/create-country.dto'
@@ -9,23 +10,32 @@ export class CountryController {
     constructor(private readonly countryService: CountryService) {}
 
     // Get Countries
+    @ApiResponse({ status: 200, description: 'Retrieve all countries.' })
     @Get()
     async getCountries(@Query() countryFilters: string): Promise<Countries[]> {
         return this.countryService.getCountries(countryFilters)
     }
 
     // Create Country
+    @ApiResponse({ status: 201, description: 'Create new country.' })
+    @ApiBody({ type: CreateCountryDto })
     @Post()
     async createCountry(@Body() countryDto: CreateCountryDto): Promise<void> {
         return this.countryService.createCountry(countryDto)
     }
 
     // Edit Country
+    @ApiResponse({ status: 200, description: 'Edit existing country.' })
+    @ApiQuery({ name: 'id', description: 'country id', required: true })
+    @ApiBody({ type: CreateCountryDto })
     @Patch()
     async editCountry(@Query('id') countryId: string, @Body() countryDto: CreateCountryDto): Promise<void> {
         return this.countryService.editCountry(countryId, countryDto)
     }
 
+    // Delete Country
+    @ApiResponse({ status: 200, description: 'Delete existing country.' })
+    @ApiQuery({ name: 'id', description: 'country id', required: true })
     @Delete()
     async deleteCountry(@Query('id') countryId: string): Promise<void> {
         return this.countryService.deleteCountry(countryId)
--- src/modules/admin/country/dto/create-country.dto.ts
+++ src/modules/admin/country/dto/create-country.dto.ts
@@ -1,11 +1,14 @@
 // Country DTO
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString } from "class-validator"
 
 export class CreateCountryDto {
+    @ApiProperty({ type: String, description: 'Country name' })
     @IsString({ message: "Country must be a string" })
     @IsNotEmpty({ message: "Country is required" })
     country: string
 
+    @ApiProperty({ type: String, description: 'Country abbreviation' })
     @IsString({ message: "Abbreviation must be a string" })
     @IsNotEmpty({ message: "Abbreviation is required" })
     abbreviation: string
