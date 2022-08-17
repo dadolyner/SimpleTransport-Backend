c4cc8924eb1788b9dd130449cde4ad4895e2cf3a
dadolyner <skulj.david@gmail.com>
2022-08-17 10:50:25 +0200

    brand documentation

--- src/modules/admin/brand/brand.controller.ts
+++ src/modules/admin/brand/brand.controller.ts
@@ -1,5 +1,6 @@
 // Brand Controller
 import { Body, Controller, Delete, Get, Patch, Post, Query } from '@nestjs/common'
+import { ApiBody, ApiQuery, ApiResponse } from '@nestjs/swagger'
 import { BrandsOutput } from 'src/interfaces/brand-output.interface'
 import { BrandService } from './brand.service'
 import { CreateBrandDto } from './dto/create-brand.dto'
@@ -9,24 +10,32 @@ export class BrandController {
     constructor(private readonly brandservice: BrandService) { }
 
     // Get Brands
+    @ApiResponse({ status: 200, description: 'Retrieve all brands' })
     @Get()
     async getBrands(@Query() brandFilters: string): Promise<BrandsOutput[]> {
         return this.brandservice.getBrands(brandFilters)
     }
 
     // Create Brand
+    @ApiResponse({ status: 201, description: 'Create a new brand' })
+    @ApiBody({ type: CreateBrandDto })
     @Post()
     async createBrand(@Body() brandDto: CreateBrandDto): Promise<void> {
         return this.brandservice.createBrand(brandDto)
     }
 
     // Edit Brand
+    @ApiResponse({ status: 200, description: 'Edit existing brand' })
+    @ApiQuery({ name: 'id', description: 'brand id', required: true })
+    @ApiBody({ type: CreateBrandDto })
     @Patch()
     async editBrand(@Query('id') brandId: string, @Body() brandDto: CreateBrandDto): Promise<void> {
         return this.brandservice.editBrand(brandId, brandDto)
     }
 
     // Delete Brand
+    @ApiResponse({ status: 200, description: 'Delete existing brand' })
+    @ApiQuery({ name: 'id', description: 'brand id', required: true })
     @Delete()
     async deleteBrand(@Query('id') brandId: string): Promise<void> {
         return this.brandservice.deleteBrand(brandId)
--- src/modules/admin/brand/dto/create-brand.dto.ts
+++ src/modules/admin/brand/dto/create-brand.dto.ts
@@ -1,11 +1,14 @@
 // Brand DTO
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString, IsUUID } from "class-validator"
 
 export class CreateBrandDto {
+    @ApiProperty({ type: String, description: 'Brand Name' })
     @IsString({ message: "Brand must be a string" })
     @IsNotEmpty({ message: "Brand is required" })
     brand: string
 
+    @ApiProperty({ type: String, description: 'Brand countryId' })
     @IsString({ message: "countryId must be a valud uuid string" })
     @IsUUID()
     countryId: string
