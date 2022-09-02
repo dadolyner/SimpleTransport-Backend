a5f29f4034790d2bfc4769879cb7faded45d2988
dadolyner <skulj.david@gmail.com>
2022-09-02 08:55:14 +0200

    mail fix

--- src/mail/rentMail.template.ts
+++ src/mail/rentMail.template.ts
@@ -9,7 +9,7 @@ const RentMailTemplate = (userThatRented: any, userThatOwns: any, car: string, s
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Forgot Password</title>
+            <title>Rented car</title>
         </head>
 
         <body>
